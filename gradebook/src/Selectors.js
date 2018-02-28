1b0c056c8d60ac96bf2120f2365fe93c1bb6dd84
calebnelson <calebnelson@gmail.com>
2018-02-28 15:20:01 -0800

    Added Lesson selector

--- gradebook/src/Selectors.js
+++ gradebook/src/Selectors.js
@@ -12,7 +12,18 @@ const classQuery = `
     }`
 
 const lessonQuery = `
-
+    query lessonsFromClass($classID: ID!) {
+        classroom(id: $classID) {
+            lessons {
+                id
+                lessonPlan {
+                    title
+                    description
+                    order
+                }
+            }
+        }
+    }
 `
 
 
@@ -25,12 +36,22 @@ function Classes(props){
     );
 }
 
+function Lessons(props){
+    return(
+        <select id="lessonSelector" key="lessonSelectorField" onChange={props.onChange}>
+            <option value="" selected disabled hidden>Select Lesson</option>
+            {props.lessons.map((data) => <option value={data.id} key={data.id}>{data.id}</option>)}
+        </select>
+    );
+}
+
 class Selectors extends Component{
     constructor(props){
         super(props);
         this.state = {
             selectedClass: undefined,
-            classes: ["Getting Classes..."]
+            classes: ["Getting Classes..."],
+            lessons: ["Choose Class to get Lessons"],
         };
         this.runClassQuery();
     }
@@ -39,6 +60,13 @@ class Selectors extends Component{
         const res = await query(classQuery);
         const queryRes = await res.json();
         this.setStateAsync({classes: queryRes.data.classrooms});
+    }
+
+    async runLessonQuery(classID){
+        var variables = "{ \"classID\": \"".concat(classID).concat("\" }");
+        const res = await query(lessonQuery, variables);
+        const queryRes = await res.json();
+        this.setStateAsync({lessons: queryRes.data.classroom.lessons})
     }
 
     setStateAsync(state){
@@ -50,9 +78,22 @@ class Selectors extends Component{
     onClassChange(newClassEvent){
         var classCode = newClassEvent.target.value;
         var index = this.state.classes.findIndex(x => x.code === classCode);
+        var sc = this.state.classes[index];
         this.setState({
-            selectedClass: this.state.classes[index]
+            selectedClass: sc
         });
+        this.runLessonQuery(sc.id);
+        console.log(this.state.lessons);
+    }
+
+    onLessonChange(newLessonEvent){
+        var lessonID = newLessonEvent.target.value;
+        var index = this.state.lessons.findIndex(x => x.id === lessonID);
+        var sl = this.state.lessons[index];
+        this.setState({
+            selectedLesson: sl
+        })
+        console.log(sl);
     }
 
     render() {
@@ -61,6 +102,10 @@ class Selectors extends Component{
                 <Classes key="classSelector"
                     classes={this.state.classes}
                     onChange={i => this.onClassChange(i)}
+                />
+                <Lessons key="lessonSelector"
+                    lessons={this.state.lessons}
+                    onChange={i => this.onLessonChange(i)}
                 />
             </div>
         );
