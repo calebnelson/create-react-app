baaf99bb0f5d8fa9cc14bf3190b051952b7dd4f5
calebnelson <calebnelson@gmail.com>
2018-03-01 16:55:31 -0800

    Added GradeTable component file

---
+++ gradebook/src/GradeTable.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {query} from './graphql'
+
+const studentsQuery = `
+
+`
+
+
+class GradeTable extends Component{
+    render() {
+        return(
+            <div>
+            </div>
+        );
+    }
+}
+export default GradeTable
