5960ae0a466161d4c63035b061b005e23d40243c
Caleb Nelson <calebnelson@gmail.com>
2018-03-07 15:16:44 -0800

    gradetable no longer logs props, was too annoying

--- gradebook/src/GradeTable.js
+++ gradebook/src/GradeTable.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 
 class GradeTable extends Component{
     render() {
-        console.log(this.props.assignment);
-        console.log(this.props.problems);
-        console.log(this.props.enrollments);
         return(
             <div>
                 <p>{JSON.stringify(this.props.assignment)}</p>
