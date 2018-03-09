0ab657a32278eb20d593ad935a6b22ef12098274
calebnelson <calebnelson@gmail.com>
2018-03-09 13:57:35 -0800

    Added input fields to grade table

--- gradebook/src/GradeTable/GradeTable.js
+++ gradebook/src/GradeTable/GradeTable.js
@@ -4,29 +4,34 @@ class GradeTable extends Component {
   render() {
     return (
       <div>
-        <table>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Total</th>
-            <th>Percent</th>
-          </tr>
-          {this.props.enrollments ? (
-            this.props.enrollments.map(data => (
-              <tr>
-                <td>{data.student.firstName}</td>
-                <td>{data.student.lastName}</td>
-                <td>0</td>
-                <td>0%</td>
-              </tr>
-            ))
-          ) : (
-            'No Students'
-          )}
-        </table>
-        <p>{JSON.stringify(this.props.assignment)}</p>
-        <p>{JSON.stringify(this.props.problems)}</p>
-        <p>{JSON.stringify(this.props.enrollments)}</p>
+        <form>
+          <table>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Total</th>
+              <th>Percent</th>
+              {this.props.problems.map(data => <th> {data.order} </th>)}
+            </tr>
+            {this.props.enrollments ? (
+              this.props.enrollments.map(data => (
+                <tr>
+                  <td>{data.student.firstName}</td>
+                  <td>{data.student.lastName}</td>
+                  <td>0</td>
+                  <td>0%</td>
+                  {this.props.problems.map(data => (
+                    <td>
+                      <input type="text" />
+                    </td>
+                  ))}
+                </tr>
+              ))
+            ) : (
+              'No Students'
+            )}
+          </table>
+        </form>
       </div>
     );
   }
