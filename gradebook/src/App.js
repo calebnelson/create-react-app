3b1dbfbbb60bec07f80c457a2aafcbc8f785f24d
Ardent Developer <dev@Brock.local>
2018-04-04 18:42:16 -0700

    Added conditional rendering

--- gradebook/src/App.js
+++ gradebook/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
 import SelectorsContainer from './Selectors/SelectorsContainer';
 import GradeTableContainer from './GradeTable/GradeTableContainer';
@@ -7,16 +8,23 @@ import GradeTableContainer from './GradeTable/GradeTableContainer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <h1>Ardent Academy Gradebook App</h1>
         <div>
           <SelectorsContainer />
           <br />
-          <GradeTableContainer />
+          {(this.props.class === null || this.props.lesson === null || this.props.assignment === null)
+            ? <h4>Choose Class, Lesson and Assignment</h4>
+            : <GradeTableContainer />}
         </div>
       </div>
     );
   }
 }
 
-export default App;
+//export default App;
+export default connect(state => ({
+  class: state.selectedClassReducer.selectedClass || null,
+  lesson: state.selectedLessonReducer.selectedLesson || null,
+  assignment: state.selectedAssignmentReducer.selectedAssignment || null,
+}))(App);
