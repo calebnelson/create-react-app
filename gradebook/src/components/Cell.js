724cc7d1a5b9232c30f576f8ce2adb342b3885aa
calebnelson <calebnelson@gmail.com>
2018-04-16 12:50:57 -0700

    changed spaces to empty strings in cells

--- gradebook/src/components/Cell.js
+++ gradebook/src/components/Cell.js
@@ -5,7 +5,7 @@ class Cell extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: this.props.defaultValue || ' ',
+      value: this.props.defaultValue || '',
     };
   }
 
@@ -20,7 +20,7 @@ class Cell extends Component {
       case ' ':
       case 'Backspace':
         this.setState({
-          value: ' ',
+          value: '',
         });
         break;
 
