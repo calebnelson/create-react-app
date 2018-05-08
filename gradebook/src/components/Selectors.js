929c13b194fa676974e1a0dae9d62e0be7b169c7
calebnelson <calebnelson@gmail.com>
2018-05-07 17:57:54 -0700

    chore: removed unneeded import

--- gradebook/src/components/Selectors.js
+++ gradebook/src/components/Selectors.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Picker } from 'react-native-web';
+import { Text, Picker } from 'react-native-web';
 import { Flex } from './Flex';
 
 function Classes(props) {
